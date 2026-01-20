9ecab0123b9a17ec949dabd9a0a04eeb13b4c65c
airchry <iampty666@gmail.com>
2026-01-21 02:05:50 +0700

    update supabase code

--- supabase.js
+++ supabase.js
@@ -1,8 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 
-const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
-);
+dotenv.config();
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
