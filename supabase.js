ee615eedf286666cb473db87c1923aad5f35c415
airchry <iampty666@gmail.com>
2026-01-21 02:13:09 +0700

    supabase

--- supabase.js
+++ supabase.js
@@ -1,10 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config(); // load environment variables
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) throw new Error("SUPABASE_URL is required.");
+if (!supabaseKey) throw new Error("SUPABASE_KEY is required.");
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
