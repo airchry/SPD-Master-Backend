7bfaafff9ac51892f39fb06b4605fb9265224dcc
airchry <iampty666@gmail.com>
2026-01-21 18:53:55 +0700

    add again

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set("trust proxy", 1);
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://https://spdmasterfront.vercel.app/"
+    "https://spdmasterfront.vercel.app/"
   ],
   credentials: true,
 }));
